06a22d8bd01590fa06b8f67ba8daeb3487d66ea9
g4gekkouga <amshumaan6pericherla@gmail.com>
2019-02-10 01:16:05 +0530

    added Catkin Workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/amshumaan/gegekkouga_print_squares/catkin_ws/src
+# Build directory: /home/amshumaan/gegekkouga_print_squares/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
